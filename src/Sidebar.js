15894ba12898d71f0bb983e101328d275e84af96
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-09-08 00:01:04 +0200

    Create Siderbar & sidebar option

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import './Sidebar.css'; 
+import './Sidebar.css';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
+import SidebarOption from './SidebarOption';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add'; 
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-      
+      <div className='sidebar__header'>
+        <div className='sidebar__info'>
+          <h2>Job Alert Platform</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            Wilfried
+          </h3>
+        </div>
+        <CreateIcon />
+      </div>
+      <SidebarOption Icon={InsertCommentIcon} title='Threads' />
+      <SidebarOption Icon={InboxIcon} title='Mentions & reactions' />
+      <SidebarOption Icon={DraftsIcon} title='Saved Items' />
+      <SidebarOption Icon={BookmarkBorderIcon} title='Channel Browser' />
+      <SidebarOption Icon={PeopleAltIcon} title='People & user groups' />
+      <SidebarOption Icon={AppsIcon} title='Apps' />
+      <SidebarOption Icon={FileCopyIcon} title='File browser' />
+      <SidebarOption Icon={ExpandLessIcon} title='Show Less' />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title='Channels' />
+      <hr />
+      <SidebarOption Icon={AddIcon} title='Add Channel' />
+
+      {/* connect to db  and list all the channels */}
+      {/* sidebaroption will be used */}
     </div>
   )
 }
